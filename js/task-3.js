c80d6d7e9c23890bcbf938cadda95e9baaa1325e
Dobryak96 <gorokhov_olexandr@ukr.net>
2024-04-19 13:45:05 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
 function filterArray(numbers, value) {
     let newArray = [];
+
     for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] > value) {
             newArray.push(numbers[i]);
         }
     }
+    
     return newArray;
 }
 
